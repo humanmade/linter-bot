f8cd96b5e23a5101302186d685e3e08e8ca0306c
Ryan McCue <me@ryanmccue.info>
2018-02-07 23:20:11 +1000

    Create an issue on activation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const { onPush, onOpenPull } = require( './hooks' );
+const { onAdd, onPush, onOpenPull } = require( './hooks' );
 
 module.exports = robot => {
+	robot.on( 'installation_repositories.added', onAdd );
 	robot.on( 'push', onPush );
 	robot.on( 'pull_request.opened', onOpenPull );
 	// robot.on( 'pull_request.synchronize', )
