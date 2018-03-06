0e1cb1adf4f5a8645a74e4e03e666f4f89cba6eb
Ryan McCue <me@ryanmccue.info>
2018-03-06 13:19:29 +1000

    Use GIST_ACCESS_TOKEN var if available

--- src/util.js
+++ src/util.js
@@ -1,5 +1,7 @@
 const githubApi = require( 'github' );
 
+const GIST_ACCESS_TOKEN = process.env.GIST_ACCESS_TOKEN || null;
+
 function combineLinters( results ) {
 	return results
 		.map( linter => linter.files )
@@ -16,7 +18,16 @@ function combineLinters( results ) {
 }
 
 const createGist = async ( description, filename, content ) => {
+	if ( ! GIST_ACCESS_TOKEN ) {
+		console.warn( 'Missing GIST_ACCESS_TOKEN for Gist creation' );
+		return null;
+	}
+
 	const anonymousGithub = new githubApi();
+	anonymousGithub.authenticate( {
+		type: 'token',
+		token: GIST_ACCESS_TOKEN
+	} );
 	const response = await anonymousGithub.gists.create( {
 		files: {
 			[ filename ]: { content },
