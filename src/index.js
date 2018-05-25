f025f41e95c47d46641aa4ffc48a7ccb6e602265
Ryan McCue <me@ryanmccue.info>
2018-05-25 16:25:12 +0100

    Disable legacy status API checks

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ module.exports = robot => {
 	robot.on( 'installation_repositories.added', onAdd );
 	robot.on( [ 'check_suite.requested', 'check_suite.rerequested' ], onCheck );
 	robot.on( 'check_run.rerequested', onCheck );
-	robot.on( 'push', onPush );
 	robot.on( 'pull_request.opened', onOpenPull );
 	robot.on( 'pull_request.synchronize', onUpdatePull );
 };
