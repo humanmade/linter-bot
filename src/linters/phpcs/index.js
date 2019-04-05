49bfb9e4c52bcf3f79c3970fc52658086dfccc86
Mike Selander <mikeselander@gmail.com>
2019-04-05 12:52:04 -0600

    Revert "Adding WordPress VIP coding standards to the list of available standards."
    This reverts commit 7d41c6301744bf38950ccf929ff10b8656c6a0b3.

--- src/linters/phpcs/index.js
+++ src/linters/phpcs/index.js
@@ -56,7 +56,7 @@ module.exports = standardPath => codepath => {
 			phpcsPath,
 			'--runtime-set',
 			'installed_paths',
-			'vendor/wp-coding-standards/wpcs,vendor/fig-r/psr2r-sniffer,vendor/humanmade/coding-standards/HM,vendor/automattic/vipwpcs',
+			'vendor/wp-coding-standards/wpcs,vendor/fig-r/psr2r-sniffer,vendor/humanmade/coding-standards/HM',
 			`--standard=${standard}`,
 			'--report=json',
 			codepath
