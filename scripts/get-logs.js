a4a23e3a0675ac7a9c06a2f13c64859dba084904
Ryan McCue <me@ryanmccue.info>
2019-05-14 18:43:34 +0100

    Add tool to get logs

---
+++ scripts/get-logs.js
@@ -0,0 +1,77 @@
+const child_process = require( 'child_process' );
+const fs = require( 'fs' );
+
+const reqId = process.argv[2];
+
+const now = Math.floor( Date.now() / 1000 );
+const start = now - ( 60 * 60 * 2 );
+const region = 'us-east-1';
+const group = '/aws/lambda/hm-linter';
+const query = `fields @message | sort @timestamp asc | filter @requestId = '${ reqId }'`;
+
+
+process.stderr.write( `Querying for ${ reqId }\n` );
+const proc = child_process.spawnSync(
+	'aws',
+	[
+		'logs',
+		'start-query',
+		'--region',
+		region,
+		'--log-group-name',
+		group,
+		'--start-time',
+		start,
+		'--end-time',
+		now,
+		'--query-string',
+		query,
+	]
+);
+
+if ( proc.status !== 0 ) {
+	console.log( '' + proc.output[2] );
+	return process.exit( 1 );
+}
+
+const { queryId } = JSON.parse( proc.output[1] );
+
+const dataRegex = /^.+?\t.+?\t(\{.+)/s;
+
+process.stderr.write( 'Waiting for results…\n' );
+setTimeout( function () {
+	const viewProc = child_process.spawnSync(
+		'aws',
+		[
+			'logs',
+			'get-query-results',
+			'--region',
+			region,
+			'--query-id',
+			queryId
+		]
+	);
+	if ( viewProc.status !== 0 ) {
+		console.log( '' + viewProc.output[2] );
+		return process.exit( 1 );
+	}
+
+	const data = JSON.parse( viewProc.output[1] );
+
+	let rawData = null;
+	data.results.slice( 0, 3 ).forEach( row => {
+		const message = row.find( f => f.field === '@message' ).value;
+
+		if ( ! rawData ) {
+			const hasMatch = message.match( dataRegex );
+			if ( hasMatch ) {
+				rawData = hasMatch[1];
+			}
+		}
+
+		process.stdout.write( message );
+	} );
+
+	fs.writeFileSync( `./${ reqId }.json`, rawData );
+	process.stderr.write( `Raw data saved to ${ reqId }.json\n` );
+}, 2000 );
