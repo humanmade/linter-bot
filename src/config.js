9328b814b0e7f4d5b67991ccbf78d7d2bb7c99ad
Mike Selander <mikeselander@gmail.com>
2019-05-30 13:07:28 -0600

    Add docblocks to config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,8 @@
+/**
+ * Default configuration.
+ *
+ * @type {Object}
+ */
 const DEFAULT_CONFIG = {
 	version: 'latest',
 	phpcs: {
@@ -9,8 +14,20 @@ const DEFAULT_CONFIG = {
 		version: 'inherit',
 	},
 };
+
+/**
+ * Filename by which to find a custom configuration in a project.
+ *
+ * @type {string}
+ */
 const FILENAME = 'hmlinter.yml';
 
+/**
+ * Merges a custom linter file (if any) with our default configuration.
+ *
+ * @param {Object} context
+ * @returns {Promise<Object|*>}
+ */
 module.exports = async context => {
 	return await context.config( FILENAME, DEFAULT_CONFIG );
 };
