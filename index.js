546ddf7315b91c48b69d1516fe666c83c17c5f48
Ryan McCue <me@ryanmccue.info>
2020-03-10 13:07:02 +0000

    Load directly from ./src
    Node 12 supports everything we need now, so we can remove Babel

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const { probot } = require( '@humanmade/probot-util' );
 // Probot setup
 const bot = probot.create();
 
-// Load Probot plugins from the `./build` folder
-bot.load( require( './build' ) );
+// Load Probot plugins from the `./src` folder
+bot.load( require( './src' ) );
 
 // Lambda Handler
 module.exports.probotHandler = probot.buildHandler( bot );
