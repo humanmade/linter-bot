c304cc5080917292f1d4c3b667ac317814f3c1d7
Ryan McCue <me@ryanmccue.info>
2020-05-22 17:39:28 +0100

    Read configuration from HEAD

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+const yaml = require( 'js-yaml' );
+const path = require( 'path' );
+
 const DEFAULT_CONFIG = {
 	version: 'latest',
 	phpcs: {
@@ -15,6 +18,37 @@ const DEFAULT_CONFIG = {
 };
 const FILENAME = process.env.CONFIG_FILE || 'hmlinter.yml';
 
-module.exports = async context => {
-	return await context.config( FILENAME, DEFAULT_CONFIG );
+/**
+ * Reads the app configuration from the given YAML file in the `.github`
+ * directory of the repository.
+ *
+ * @internal Ported from probot, but adapted to read from the current branch.
+ *
+ * @param context Context from Probot
+ * @param head SHA of the head commit we're running against.
+ * @return Configuration object read from the file
+ */
+module.exports = async ( context, head ) => {
+	const params = {
+		...context.repo( {
+			path: path.posix.join( '.github', FILENAME ),
+			foo: 'bar',
+		} ),
+		ref: head,
+	};
+
+	try {
+		const res = await context.github.repos.getContent( params );
+		const config = yaml.safeLoad( Buffer.from( res.data.content, 'base64' ).toString() ) || {};
+		return {
+			...defaultConfig,
+			...config
+		};
+	} catch ( err ) {
+		if ( err.code === 404 ) {
+			return DEFAULT_CONFIG;
+		} else {
+			throw err;
+		}
+	}
 };
