27f1e17e7191e6ec13dcf9972bc78a3e9ab7f808
Faishal Saiyed <saiyedfaishal@gmail.com>
2021-05-06 16:03:11 -0700

    Fix annotation chunk issue : Update check in look to post all annotations

--- src/hooks.js
+++ src/hooks.js
@@ -226,25 +226,27 @@ const onCheck = async context => {
 			throw e;
 		}
 
-		let annotations;
+		let annotationGroups;
 		if ( process.env.CHECK_ANNOTATION_ONLY_RELATED ) {
 			const currentAnnotations = formatAnnotations( lintState, `https://github.com/${owner}/${repo}/blob/${head_sha}`, diffMapping );
 
 			// Push annotations 50 at a time (and send the leftovers with the completion).
-			const annotationGroups = _chunk( currentAnnotations, 50 );
-			annotations = annotationGroups.pop();
+			annotationGroups = _chunk( currentAnnotations, 50 );
 		}
 
 		const summary = formatSummary( lintState );
 		const fullSummary = summary + `\n\n[View output](${ gistUrl })`;
-		completeRun(
-			'neutral',
-			{
-				title: lintState.passed ? 'All checks passed' : `Ignored ${ summary }`,
-				summary: fullSummary,
-				annotations,
-			}
-		);
+		// To create more than 50 annotations, we have to make multiple requests to the Update a check run 
+		annotationGroups.forEach( annotationGroup => {
+			completeRun(
+				'neutral',
+				{
+					title: lintState.passed ? 'All checks passed' : `Ignored ${ summary }`,
+					summary: fullSummary,
+					annotations: annotationGroup,
+				}
+			);
+		} )
 	} else if ( lintState.passed ) {
 		completeRun(
 			'success',
