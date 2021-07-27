4237c58656d9a42211bacc19d93033a4f6b52f57
Faishal Saiyed <saiyedfaishal@gmail.com>
2021-07-27 16:28:40 -0700

    remove specific repo directory instead of all repos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
+const fs = require( 'fs' );
+const path = require( 'path' );
 const pify = require( 'pify' );
 const rimraf = require( 'rimraf' );
+const realpath = pify( fs.realpath );
 
 const { onAdd, onCheck, onPush, onOpenPull, onUpdatePull } = require( './hooks' );
 
-const clean = async () => {
+const clean = async ( context ) => {
 	// Clean up the tmp directories.
 	const TEMP_DIR = process.env.TEMP_DIR || '/tmp'
-	console.log( `Cleaning up ${ TEMP_DIR }` );
+	const { payload } = context;
+
+	let { head_sha } = payload.check_suite || payload.check_run.check_suite;
+
+	if ( ! head_sha && payload.pull_request ){
+		head_sha = payload.pull_request.head.sha;
+	}
+
+	const owner = payload.repository.owner.login;
+	const repo = payload.repository.name;
+	const extractDir = path.join( await realpath( `${ TEMP_DIR }/repos` ), `${owner}-${repo}-${head_sha}` );
+
+	console.log( `Cleaning up ${ extractDir }` );
+
 	const rmrf = pify( rimraf );
+	await rmrf( extractDir );
 	await rmrf( `${ TEMP_DIR }/downloads` );
-	await rmrf( `${ TEMP_DIR }/repos` );
 };
-const withClean = func => ( ...args ) => func( ...args ).finally( () => clean() );
+
+const withClean = func => ( args ) => {
+	const context = args;
+	func( args ).finally( () => {
+		clean( context );
+	});
+}
 
 module.exports = robot => {
 	robot.on( 'installation_repositories.added', withClean( onAdd ) );
